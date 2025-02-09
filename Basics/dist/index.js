5411d9ab9fd7538d638f7e55d8ad090ccd5c53eb
Shyam <gshyamprasad2001@gmail.com>
2025-02-09 08:23:00 +0530

    basics

---
+++ Basics/dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+// /==========================================================//Arrays// ===============================================
+let fruits = ["mango", "orange", "peach", "kiwi"];
+let scores = [12, 32, 45, 53, 77, 455, 65.345];
+//Array Methods
+//==========Mixed Type ================//
+let nature = ["hello", 7];
+// console.log(nature);
---
+++ Basics/src/index.ts
@@ -0,0 +1,13 @@
+
+// /==========================================================//Arrays// ===============================================
+
+let fruits:string[] = ["mango","orange","peach","kiwi"];
+let scores:number[] = [12,32,45,53,77,455,65.345]
+
+//Array Methods
+
+
+//==========Mixed Type ================//
+
+let nature:[string,number] = ["hello",7]
+// console.log(nature);
