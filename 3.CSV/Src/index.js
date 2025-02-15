400ca93fb910adbefecd356ae7e8b5ff6ea4e3c9
Shyam <gshyamprasad2001@gmail.com>
2025-02-15 23:05:28 +0530

    csv

--- 3.CSV/Src/index.js
+++ 3.CSV/Src/index.js
@@ -5,6 +5,19 @@
 class CsvWriter {
     constructor(columns) {
         this.columns = columns;
+        this.csv = this.columns.join(",") + "\n";
+    }
+    addRows(values) {
+        const rows = values.map((v) => this.formatRows(v));
+        this.csv += rows.join("\n") + "\n";
+        console.log(this.csv);
+    }
+    formatRows(p) {
+        return this.columns.map((col) => p[col]).join(",");
     }
 }
-const CsvFile = new CsvWriter(['id', 'amount', 'to', 'notes']);
+const csvFile = new CsvWriter(["id", "amount", "to", "notes"]);
+csvFile.addRows([
+    { id: 1, amount: 50, to: "Shyam", notes: "for saving the world" },
+    { id: 2, amount: 502, to: "Naidu", notes: "for being awesome" },
+]);
--- 3.CSV/Src/index.ts
+++ 3.CSV/Src/index.ts
@@ -4,20 +4,36 @@
 //==CSV PROJECT==
 //=================
 
-interface Payment{
-    id:number
-    amount:number
-    to :string
-    notes:string
+interface Payment {
+  id: number;
+  amount: number;
+  to: string;
+  notes: string;
 }
 
-type Paymentcolumns = ('id'|'amount'|'to'|'notes')[]
+type PaymentColumns = (keyof Payment)[];
 
 class CsvWriter {
-    constructor(private columns :Paymentcolumns ) {
+  private csv: string;
 
-    }
-    private csv :string
+  constructor(private columns: PaymentColumns) {
+    this.csv = this.columns.join(",") + "\n";
+  }
+
+  addRows(values: Payment[]): void {
+    const rows = values.map((v) => this.formatRows(v));
+    this.csv += rows.join("\n") + "\n";
+    console.log(this.csv);
+  }
+
+  private formatRows(p: Payment): string {
+    return this.columns.map((col) => p[col as keyof Payment]).join(",");
+  }
 }
 
-const CsvFile = new CsvWriter(['id','amount','to','notes'])
+const csvFile = new CsvWriter(["id", "amount", "to", "notes"]);
+
+csvFile.addRows([
+  { id: 1, amount: 50, to: "Shyam", notes: "for saving the world" },
+  { id: 2, amount: 502, to: "Naidu", notes: "for being awesome" },
+]);
