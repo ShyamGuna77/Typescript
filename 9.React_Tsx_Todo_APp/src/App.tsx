77dec324d81ecbd0ff8800317949bf9ea1d1fa90
Shyam <gshyamprasad2001@gmail.com>
2025-02-22 13:10:20 +0530

    App

--- 9.React_Tsx_Todo_APp/src/App.tsx
+++ 9.React_Tsx_Todo_APp/src/App.tsx
@@ -1,17 +1,24 @@
-import Todos from "./components/Todos"
+import TodoForm from "./components/TodoForm";
+import TodoList from "./components/TodoList";
+import { TodoProvider } from "./store/TodoContext";
 
 function App() {
 
 
   return (
     <>
+
+    <TodoProvider>
     
-      <div className="p-4 m-8 flex items-center justify-center max-w-md mx-auto bg-violet-100 rounded-lg shadow-lg">
+      <div className="p-4 m-8 flex items-center justify-center  bg-violet-100 rounded-lg shadow-lg">
         <h1 className="font-bold text-2xl text-pink-500">React Typescript Todo App</h1>
-        <Todos />
+        <TodoList />
+        <TodoForm />
       </div>
+      </TodoProvider>
+      
     </>
   );
 }
 
-export default App
+export default App;
