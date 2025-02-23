f889c7d63af78df3b0ba375c0cc0fb93316b4b90
Shyam <gshyamprasad2001@gmail.com>
2025-02-23 11:00:19 +0530

    Added Timers

--- 7.ContextApi/src/components/Timer.tsx
+++ 7.ContextApi/src/components/Timer.tsx
@@ -1,5 +1,6 @@
+import { useTimersContext } from '../store/Timers-context.tsx';
 import Container from './UI/Container.tsx';
-
+import { useEffect, useRef, useState } from 'react';
 
 
 type TimersProps ={
@@ -7,10 +8,38 @@ type TimersProps ={
     duration:number;
 }
 export default function Timer({name,duration}:TimersProps) {
+
+  const inputInterval = useRef<number | null>(null)
+ const [remainingTime,setRemainingTime] = useState(duration*1000)
+
+ const {isRunning} = useTimersContext()
+
+ if(remainingTime<= 0 && inputInterval.current) {
+ clearInterval(inputInterval.current)
+ }
+
+ useEffect(()=>{
+  let interval:number
+  if(isRunning){
+
+     interval = setInterval(()=>{
+      setRemainingTime(prevTime => prevTime-50)
+    },50)
+    inputInterval.current = interval;
+  } else if (inputInterval.current ){
+    clearInterval(inputInterval.current)
+  }
+
+   return ()=>clearInterval(interval)  
+ },[isRunning])
+
+ const formattedTime = (remainingTime/1000).toFixed(2)
+ 
   return (
     <Container as="article">
       <h2>{name}</h2>
-      <p>{duration}</p>
+      <p><progress max = {duration*1000} value={remainingTime} /> </p>
+      <p>{formattedTime}</p>
     </Container>
   );
 }
